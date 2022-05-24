25b11fd32c247854260e3bcecad7a57643e9c5bd
andrii-ostapov <ost.and.g@gmail.com>
2022-05-24 23:02:21 +0300

    Create post list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import React, {useState} from 'react';
+// import Counter from "./components/Counter";
+// import ClassCounter from "./components/ClassCounter";
 import './styles/app.css'
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
+    const [posts, setPosts] = useState( [
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'}
+    ]);
 
   return (
     <div className="App">
+        <PostList posts={posts} title='List of posts JS'/>
         {/*<ClassCounter/>*/}
         {/*<Counter/>*/}
-        <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}} />
     </div>
   );
 }
