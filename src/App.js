87fb3ae31e49b25b3110dba9fbcfea2ac904d811
andrii-ostapov <ost.and.g@gmail.com>
2022-05-27 23:36:31 +0300

    Create components and style modules

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, {useState} from 'react';
 // import Counter from "./components/Counter";
 // import ClassCounter from "./components/ClassCounter";
 import './styles/app.css'
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState( [
@@ -11,9 +12,24 @@ function App() {
         {id: 2, title: 'Javascript 2', body: 'Description'},
         {id: 3, title: 'Javascript 3', body: 'Description'}
     ]);
+    const [title, setTitle] = useState()
+
+    const addNewPost = () => {
+        console.log(title)
+    }
 
   return (
     <div className="App">
+        <form>
+            <MyInput
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+            <MyInput type="text" placeholder="Description"/>
+            <MyButton onClick={addNewPost}/>
+        </form>
         <PostList posts={posts} title='List of posts JS'/>
         {/*<ClassCounter/>*/}
         {/*<Counter/>*/}
