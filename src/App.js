4c379bc0358518c1c9d1df8442f3684d2b19d45a
andrii-ostapov <ost.and.g@gmail.com>
2022-05-31 23:11:27 +0300

    Sort optimization

--- src/App.js
+++ src/App.js
@@ -14,7 +14,16 @@ function App() {
         {id: 2, title: 'sss 2', body: 'iii'},
         {id: 3, title: 'ppppp 3', body: 'qqqqq'}
     ]);
-    const [selectedSort, setSelectedSort] = useState('')
+    const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+
+    function getSortedPosts() {
+        if(selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+        }
+        return posts
+    }
+    const sortedPosts = getSortedPosts();
 
     // const bodyInputRef = useRef();
     const createPost = (newPost) => {
@@ -27,7 +36,6 @@ function App() {
     
     const sortPosts = (sort) => {
       setSelectedSort(sort);
-      setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
     }
 
   return (
@@ -35,6 +43,11 @@ function App() {
         <PostForm create={createPost}/>
         <hr style={{margin: '15px 0'}}/>
         <div>
+            <MyInput
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+                placeholder='Search...'
+            />
             <MySelect
                 value={selectedSort}
                 onChange={sortPosts}
@@ -47,7 +60,7 @@ function App() {
         </div>
         {posts.length
             ?
-            <PostList remove={removePost} posts={posts} title='List of posts JS'/>
+            <PostList remove={removePost} posts={sortedPosts} title='List of posts JS'/>
             :
             <h1 style={{textAlign: 'center'}}>No posts not found!</h1>
         }
