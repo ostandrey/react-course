7b5cafcbab66206718a7f44c31abd964af83de1a
andrii-ostapov <ost.and.g@gmail.com>
2022-06-06 23:31:46 +0300

    Add error handling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, {useEffect, useMemo, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 // import Counter from "./components/Counter";
 // import ClassCounter from "./components/ClassCounter";
 import './styles/app.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import {usePosts} from "./hooks/usePost";
-import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
     const [posts, setPosts] = useState( []);
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    const [isPostsLoading, setIsPostsLoading] = useState(false);
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    })
 
     useEffect(() => {
         fetchPosts();
@@ -31,20 +32,12 @@ function App() {
         setModal(false)
     }
 
-    async function fetchPosts() {
-        setIsPostsLoading(true);
-        const posts = await PostService.getAll();
-        setPosts(posts);
-        setIsPostsLoading(false);
-    }
-
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
   return (
     <div className="App">
-        <button onClick={fetchPosts}>Get posts</button>
         <MyButton
             style={{marginTop: 30}}
             onClick={() => setModal(true)}
@@ -59,6 +52,9 @@ function App() {
             filter={filter}
             setFilter={setFilter}
         />
+        {
+            postError && <h1>Error: ${postError}</h1>
+        }
         {isPostsLoading
             ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
             : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='List of posts JS'/>
