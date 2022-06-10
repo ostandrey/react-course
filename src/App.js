12e00c86e6fa9b66cdc34a3dd527f6b00cb3099c
andrii-ostapov <ost.and.g@gmail.com>
2022-06-10 23:21:12 +0300

    Create navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './styles/app.css'
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route, Routes, Navigate} from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Navbar from "./components/UI/navbar/Navbar";
+import Error from "./pages/Error";
 
 function App() {
     return (
         <Router>
+            <Navbar/>
             <Routes>
                 <Route path='/about' element={<About />}/>
                 <Route path="/posts"  element={<Posts />}/>
+                <Route path="/error"  element={<Error />}/>
+                <Route path='*' element={<Error/>}/>
             </Routes>
         </Router>
     )
