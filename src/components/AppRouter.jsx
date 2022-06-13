0880a2e33fb4ba07ca05f308d0cd4f0f7f12b1cc
andrii-ostapov <ost.and.g@gmail.com>
2022-06-13 23:13:37 +0300

    Create login page

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,16 +4,40 @@ import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
 import PostIdPage from "../pages/PostIdPage";
+import {privateRoutes, publicRoutes} from "../router";
+import Login from "../pages/Login";
 
 const AppRouter = () => {
+    const isAuth = true
     return (
-        <Routes>
-            <Route path='/about' element={<About />}/>
-            <Route exact path="/posts"  element={<Posts />}/>
-            <Route exact path="/posts/:id"  element={<PostIdPage />}/>
-            <Route path="/error"  element={<Error />}/>
-            <Route path='*' element={<Error/>}/>
-        </Routes>
+        isAuth
+        ?
+            <Routes>
+                {/*{privateRoutes.map(route =>*/}
+                {/*    <Route*/}
+                {/*        path={route.path}*/}
+                {/*        element={route.component}*/}
+                {/*        exact={route.exact}*/}
+                {/*    />*/}
+                {/*)}*/}
+                <Route path='/about' element={<About />}/>
+                <Route exact path="/posts"  element={<Posts />}/>
+                <Route exact path="/posts/:id"  element={<PostIdPage />}/>
+                <Route path="/error"  element={<Error />}/>
+                <Route path='*' element={<Posts/>}/>
+            </Routes>
+            :
+            <Routes>
+                {/*{publicRoutes.map(route =>*/}
+                {/*    <Route*/}
+                {/*        path={route.path}*/}
+                {/*        element={route.component}*/}
+                {/*        exact={route.exact}*/}
+                {/*    />*/}
+                {/*)}*/}
+                <Route path='/login' element={<Login />}/>
+            </Routes>
+
     );
 };
 
